99d2c5ffaf0f2a480f11c67698e7158584692e8e
Scott Sheaf <scottsheaf@gmail.com>
2021-11-29 21:14:46 -0500

    convert static pages to handlebars format for templating.  add HBS support

--- src/app.js
+++ src/app.js
@@ -4,19 +4,42 @@ const express = require('express');
 
 const app = express();
 
+// set express variables
+// setup express to use the hbs templating engine
+app.set('view engine','hbs');
+
 // tell express where to find static content
-// root url route also comes from here
 app.use(express.static(path.join(__dirname, '../public')));
 
+// empty route
+app.get('', (req, res) => {
+    // express knows everything it needs to given 
+    // the rendering engine set up above
+    // the second parameter is an object that contains
+    // all the data we want the HTML to have access to
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Scott Sheaf'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+
+    });
+});
+
 app.get('/weather', (req, res) => {
-
     res.send('Weather Page');
-
 });
 
 app.listen(3000, () => {
-
     console.log("Server Running on Port 3000");
-
 });
 
