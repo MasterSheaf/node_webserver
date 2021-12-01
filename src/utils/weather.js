d73de86ba5e69ca3f00e98fd186786f2cf9b39b9
Scott Sheaf <scottsheaf@gmail.com>
2021-11-30 22:52:53 -0500

    clean up

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -1,23 +1,5 @@
 const request = require('request')
 
-// const url = 'http://api.weatherstack.com/current?access_key=6ebcb1a94a8e01ad5aed80366d95ce80&query=43065&units=f';
-
-
-// request({url: url, json: true}, function (error, response, data) {
-
-//     if (error !== null) {
-//         console.error("error:", error); // Print the error if one occurred
-//         return;
-//     }
-
-//     console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-
-//     console.log("body:", data);
-//     console.log("Temp = " + data.current.temperature + " deg F");
-// });
-
-
-
 const weather = (lat, lon, callback) => {
 
     var url = 'http://api.weatherstack.com/current?access_key=6ebcb1a94a8e01ad5aed80366d95ce80';
